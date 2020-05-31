655b74d9277a1162409cd84bb93e69e2e86f07c6
Dennis Nienhüser <dennis@nienhueser.de>
2020-05-31 13:27:08 +0200

    Download directly to target filename

--- docker/download.bash
+++ docker/download.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 version="$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev)"
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/"$version"/NVIDIA-Linux-x86_64-"$version".run
-mv NVIDIA-Linux-x86_64-"$version".run NVIDIA-DRIVER.run
-
+wget "http://us.download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run" -O NVIDIA-DRIVER.run
